34818c9389f5425ffbed578699741bbcb99608fe
unknown <halfmoon1.0dl@gmail.com>
2021-10-09 17:43:26 +0700

    Tichhop EF

---
+++ Models/Article.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorWeb.Models
+{
+    public class Article
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [Column(TypeName="nvarchar")]
+        [StringLength(255)]
+        public string Title { get; set; }
+        [DataType(DataType.Date)]
+        [Required]
+        public DateTime  Created { get; set; }
+        [Column(TypeName = "ntext")]
+        public string Content { get; set; }
+    }
+}
---
+++ Models/MyBlogContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorWeb.Models
+{
+    public class MyBlogContext : DbContext
+    {
+        public MyBlogContext(DbContextOptions<MyBlogContext> options) : base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            base.OnConfiguring(builder);
+        }
+       protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<Article> articles { set; get; }
+    }
+}
